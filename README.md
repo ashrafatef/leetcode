6203129991fd55d0ae725f0fbe15e8c147aa4c4a
ashrafatef <ashraaf.ateef@gmail.com>
2023-09-03 15:54:18 +0200

    init commit with longestCommonPrefix problem solved

---
+++ README.md
@@ -0,0 +1,5 @@
+## LeetCode Problem Solving (playing)
+Here is you will find my playing by problem solving. using golang 
+
+List of Problems 
+- [Longest Common Prefix ](./longestCommonPrefix)
---
+++ longestCommonPrefix.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func longestCommonPrefix(strs []string) string {
+	prefix := ""
+	flag := true
+	index := 0
+	character := ""
+	for flag == true && len(strs) != 0 {
+		for v := 0; v < len(strs); v++ {
+			if len(strs[v]) <= index {
+				flag = false
+				break
+			}
+			if v == 0 {
+				character = string(strs[v][index])
+			}
+			if string(strs[v][index]) != string(character) {
+				flag = false
+				break
+			}
+		}
+		prefix = prefix + string(character)
+		index++
+	}
+	if len(prefix) == 0 {
+		return ""
+	}
+	if flag == false {
+		return prefix[:len(prefix)-1]
+	}
+	return prefix
+}
+func main() {
+	response := longestCommonPrefix([]string{""})
+	fmt.Printf("%+v\n", response)
+}
